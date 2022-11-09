1b398a9bdf5e89b749962b68d2b203ef5fa579ab
Joe Owen <joe.a.owen@gmail.com>
2022-11-08 18:36:30 -0800

    Birthday selection UI

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
 import React from 'react'
+import logo from './images/logo.png'
 
 function App() {
   return (
     <div className="App">
-      <h1>International to Korean age converter</h1>
+      <div className='header'>
+        <img src={logo} alt="logo" className="logo"></img>
+        <h1>Age Converter</h1>
+      </div>
+      <div className="container">
+        <div className="form">
+          <h2>Birthday</h2>
+          <input type="date" id="birthday" name="birthday"></input>
+          <button>Convert</button>
+        </div>
+      </div>
+      <div className="footer">
+        <h3>Created by</h3>
+        <p><i>Amy Martin | Joe Owen</i></p>
+      </div>
     </div>
   )
 }
